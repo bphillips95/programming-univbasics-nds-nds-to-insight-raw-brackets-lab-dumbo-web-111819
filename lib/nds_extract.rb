80e3cfaaf5dd833cd2d03cdb0690baa087ab9bb3
Baruch Phillips <baruchphillips@gmail.com>
2019-11-08 04:50:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,22 +36,4 @@ end
 
 
   # add directors_database[0][:movies][row_index][ :worldwide_gross]
-# runs through column_index and after row_index
-
-
-
-
-# def movie_one
-# total_gross = 0
-# this_movie = 0 
-#   while this_movie < directors_database[0][:movies].count do
-#   director_person = 0 
-# while director_person < directors_database.count do
-# total_gross+= directors_database[0][:movies][this_movie][:worldwide_gross]
-# this_movie+=1 
-# end
-# return total_gross
-# end
-# puts movie_one
-# movie_one gives total gross of Steven Spielberg
 # inner loop loops through films then adds them, outer loop loops through directors
